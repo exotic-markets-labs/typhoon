1a0a4e542fc1b1bbad15ca42bcb3a4bcc8dda38b
Jean Marchand <jeanno11@orange.fr>
2024-12-26 16:17:00 +0100

    Update test for filter_by_impls

--- crates/metadata-extractor/tests/filter_by_impls_visitor.rs
+++ crates/metadata-extractor/tests/filter_by_impls_visitor.rs
@@ -1,43 +1,39 @@
 use {
-    codama_attributes::Attributes,
+    codama::{KorokTrait, KorokVisitor, PublicKeyTypeNode, UniformVisitor},
     codama_korok_visitors::KorokVisitable,
-    codama_koroks::{CrateKorok, ItemKorok},
-    codama_stores::CrateStore,
-    std::path::PathBuf,
-    syn::{parse_quote, File},
+    codama_koroks::ItemKorok,
+    syn::{parse_quote, Item},
     typhoon_metadata_extractor::visitors::FilterByImplsVisitor,
 };
 
 #[test]
 fn it_apply_marker_node() {
-    let parsed: File = parse_quote! {
-        pub struct RandomState {}
+    let ast: Item = parse_quote! {
+        mod program {
+            pub struct RandomState {}
 
-        impl Owner for RandomState {
-            const OWNER: Pubkey = crate::ID;
+            impl Owner for RandomState {
+                const OWNER: Pubkey = crate::ID;
+            }
         }
     };
-    let another_file = parsed.clone();
-    let items = ItemKorok::parse_all(&another_file.items, &[], &mut 0).unwrap();
+    let mut korok = ItemKorok::parse(&ast, &[], &mut 0).unwrap();
 
-    let mut korok = CrateKorok {
-        attributes: Attributes(vec![]),
-        items,
-        node: None,
-        store: &CrateStore {
-            file: parsed,
-            manifest: None,
-            file_modules: vec![],
-            path: PathBuf::new(),
-        },
+    korok.accept(&mut FilterByImplsVisitor::new(
+        &["Owner"],
+        UniformVisitor::new(|mut k, visitor| {
+            visitor.visit_children(&mut k);
+            k.set_node(Some(PublicKeyTypeNode::new().into()));
+        }),
+    ));
+
+    let ItemKorok::Module(module) = &korok else {
+        panic!("Expected program module");
     };
 
-    let mut visitor = FilterByImplsVisitor::new(&["Owner"]);
-    korok.accept(&mut visitor);
+    let ItemKorok::Struct(random_state) = &module.items[0] else {
+        panic!("Expected RandomState struct");
+    };
 
-    println!("{visitor:?}")
-    // println!("{korok:?}");
-    // assert!(korok.iter().len() == 1)
-
-    // korok.acc
+    assert_eq!(random_state.node, Some(PublicKeyTypeNode::new().into()));
 }
