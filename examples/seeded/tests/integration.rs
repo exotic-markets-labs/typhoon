ef22d8f5e564aaf35187a7bc2c4daae6948a7f06
Dodecahedr0x <hexadecifish@gmail.com>
2024-12-27 22:03:23 +0100

    Fix deploy path

--- examples/seeded/tests/integration.rs
+++ examples/seeded/tests/integration.rs
@@ -16,7 +16,7 @@ use {
 
 fn read_program() -> Vec<u8> {
     let mut so_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-    so_path.push("../../target/deploy/seeded.so");
+    so_path.push("target/deploy/seeded.so");
 
     std::fs::read(so_path).unwrap()
 }
