67a7288596f281a140a1e947b45d08023e1f1d3d
Jean Marchand <jeanno11@orange.fr>
2024-12-28 14:48:44 +0100

    Add more items to the filter

--- crates/metadata-extractor/src/visitors/mod.rs
+++ crates/metadata-extractor/src/visitors/mod.rs
@@ -1,4 +1,9 @@
+mod cache_by_impls_visitor;
 mod filter_by_impls_visitor;
+mod filter_items_visitor;
 mod set_program_id_visitor;
 
-pub use {filter_by_impls_visitor::*, set_program_id_visitor::*};
+pub use {
+    cache_by_impls_visitor::*, filter_by_impls_visitor::*, filter_items_visitor::*,
+    set_program_id_visitor::*,
+};
