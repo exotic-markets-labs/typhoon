85d08981735338c502a74705f1eb7ca4a933494f
Jean Marchand <jeanno11@orange.fr>
2024-12-28 14:48:44 +0100

    Add more items to the filter

---
+++ crates/metadata-extractor/src/visitors/cache_by_impls_visitor.rs
@@ -0,0 +1,49 @@
+use std::{borrow::BorrowMut, collections::HashSet};
+
+use codama::KorokVisitor;
+use codama_syn_helpers::extensions::{PathExtension, TypeExtension};
+use syn::Item;
+
+pub struct CacheByImplsVisitor<'a> {
+    traits: &'a [&'static str],
+    cache: HashSet<String>,
+}
+
+impl<'a> CacheByImplsVisitor<'a> {
+    pub fn new(traits: &'a [&'static str]) -> Self {
+        CacheByImplsVisitor {
+            traits,
+            cache: HashSet::new(),
+        }
+    }
+
+    pub fn get_cache(&mut self) -> Vec<String> {
+        self.cache.drain().collect()
+    }
+}
+
+impl KorokVisitor for CacheByImplsVisitor<'_> {
+    fn visit_unsupported_item(&mut self, korok: &mut codama_koroks::UnsupportedItemKorok) {
+        self.visit_children(korok);
+
+        let Item::Impl(impl_item) = korok.ast else {
+            return;
+        };
+
+        let Some((_, trait_path, _)) = &impl_item.trait_ else {
+            return;
+        };
+
+        if !self
+            .traits
+            .iter()
+            .any(|trait_name| trait_path.last().ident == trait_name)
+        {
+            return;
+        }
+
+        if let Ok(impl_path) = impl_item.self_ty.as_path() {
+            self.cache.borrow_mut().insert(impl_path.last_str());
+        }
+    }
+}
