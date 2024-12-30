a54c2803ea46ea24d6d826aea527317cfd809461
Jean Marchand <jeanno11@orange.fr>
2024-12-30 16:18:25 +0100

    Add account node

--- crates/metadata-extractor/src/plugin.rs
+++ crates/metadata-extractor/src/plugin.rs
@@ -1,9 +1,13 @@
 use {
     crate::{
         helpers::ItemHelper,
-        visitors::{CacheByImplsVisitor, FilterItemsVisitor, SetProgramIdVisitor},
+        visitors::{CacheByImplsVisitor, SetProgramIdVisitor},
     },
-    codama::{CombineModulesVisitor, SetProgramMetadataVisitor},
+    codama::{
+        AccountNode, CombineModulesVisitor, CombineTypesVisitor, FilterItemsVisitor, KorokMut,
+        KorokTrait, KorokVisitor, NestedTypeNode, Node, SetProgramMetadataVisitor, StructTypeNode,
+        UniformVisitor,
+    },
     codama_korok_plugins::KorokPlugin,
     codama_korok_visitors::{
         ComposeVisitor, KorokVisitable, SetBorshTypesVisitor, SetLinkTypesVisitor,
@@ -18,16 +22,9 @@ impl KorokPlugin for TyphoonPlugin {
 
         let mut cache = CacheByImplsVisitor::new(&["Owner"]);
         visitable.accept(&mut cache);
+        let account_cache = cache.get_cache();
 
-        let struct_cache = cache.get_cache();
-
-        let compose_visitor = || {
-            ComposeVisitor::new()
-                .add(SetBorshTypesVisitor::new())
-                .add(SetLinkTypesVisitor::new())
-        };
-
-        let cache_slice = struct_cache.as_slice();
+        let cache_slice = account_cache.as_slice();
         let mut default_visitor = ComposeVisitor::new()
             .add(FilterItemsVisitor::new(
                 move |item| {
@@ -35,20 +32,43 @@ impl KorokPlugin for TyphoonPlugin {
                         || item.has_attribute("account")
                         || item.has_attribute("context")
                 },
-                compose_visitor(),
+                ComposeVisitor::new()
+                    .add(SetBorshTypesVisitor::new())
+                    .add(SetLinkTypesVisitor::new())
+                    .add(CombineTypesVisitor::new()),
+            ))
+            .add(FilterItemsVisitor::new(
+                move |item| item.has_name_in_cache(cache_slice) || item.has_attribute("account"),
+                UniformVisitor::new(|mut k, visitor| {
+                    visitor.visit_children(&mut k);
+                    apply_account(k);
+                }),
             ))
             .add(SetProgramIdVisitor::new())
             .add(SetProgramMetadataVisitor::new())
-            .add(FilterItemsVisitor::new(
-                move |item| {
-                    item.has_name_in_cache(cache_slice)
-                        || item.has_attribute("account")
-                        || item.has_attribute("context")
-                },
-                compose_visitor(),
-            ))
             .add(CombineModulesVisitor::new());
 
         visitable.accept(&mut default_visitor);
     }
+}
+
+fn apply_account(mut korok: KorokMut) {
+    let Some(Node::DefinedType(ref def_ty)) = korok.node() else {
+        return;
+    };
+
+    let Ok(data) = NestedTypeNode::<StructTypeNode>::try_from(def_ty.r#type.clone()) else {
+        return;
+    };
+
+    let account = AccountNode {
+        name: def_ty.name.clone(),
+        docs: def_ty.docs.clone(),
+        size: None,
+        pda: None,
+        discriminators: Vec::new(),
+        data,
+    };
+
+    korok.set_node(Some(Node::Account(account)));
 }
