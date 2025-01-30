c4ddb0349778cfd28ef76be06773f5ab7a271cc7
Jean Marchand <jeanno11@orange.fr>
2025-01-30 16:31:25 +0100

    Add optional account

---
+++ crates/accounts/src/accounts/option.rs
@@ -0,0 +1,16 @@
+use typhoon_program::{program_error::ProgramError, pubkey::Pubkey, RawAccountInfo};
+
+use crate::{FromAccountInfo, ReadableAccount};
+
+impl<'a, T> FromAccountInfo<'a> for Option<T>
+where
+    T: FromAccountInfo<'a> + ReadableAccount,
+{
+    fn try_from_info(info: &'a RawAccountInfo) -> Result<Self, ProgramError> {
+        if info.key() == &Pubkey::default() {
+            Ok(None)
+        } else {
+            T::try_from_info(info).map(Some)
+        }
+    }
+}
