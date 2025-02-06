ad2b3053820123585433349b26b475a78b59d4c4
Jean Marchand <jeanno11@orange.fr>
2025-02-06 18:54:43 +0100

    Improve account initialization

--- crates/lib/src/lib.rs
+++ crates/lib/src/lib.rs
@@ -14,6 +14,9 @@ pub mod lib {
 pub mod prelude {
     pub use {
         super::{lib::*, macros::*, typhoon_program},
-        typhoon_program::{msg, program_error::ProgramError, pubkey::Pubkey, ProgramResult},
+        typhoon_program::{
+            msg, program_error::ProgramError, pubkey::Pubkey, sysvars::clock::Clock,
+            sysvars::rent::Rent, sysvars::Sysvar, ProgramResult,
+        },
     };
 }
