c673e80c77e7a03e11e4b9136992cb4f7e08e6f0
Jean Marchand <jeanno11@orange.fr>
2025-03-26 10:08:51 +0700

    Rebase and fix the version

--- crates/metadata-extractor/src/plugin.rs
+++ crates/metadata-extractor/src/plugin.rs
@@ -1,18 +1,5 @@
 use {
-    crate::{
-        helpers::ItemHelper,
-        visitors::{CacheByImplsVisitor, CacheInstructionIdents, SetProgramIdVisitor},
-    },
-    codama::{
-        AccountNode, CodamaResult, CombineModulesVisitor, CombineTypesVisitor, FilterItemsVisitor,
-        KorokMut, KorokTrait, KorokVisitor, NestedTypeNode, Node, SetProgramMetadataVisitor,
-        StructTypeNode, UniformVisitor,
-    },
-    codama_korok_plugins::KorokPlugin,
-    codama_korok_visitors::{
-        ComposeVisitor, KorokVisitable, SetBorshTypesVisitor, SetLinkTypesVisitor,
-    },
-    std::collections::HashSet,
+    codama::CodamaResult, codama_korok_plugins::KorokPlugin, codama_korok_visitors::KorokVisitable,
 };
 
 pub struct TyphoonPlugin;
@@ -25,71 +12,71 @@ impl KorokPlugin for TyphoonPlugin {
     ) -> CodamaResult<()> {
         next(visitable)?;
 
-        let mut cache_accounts = HashSet::new();
-        let mut cache_instructions = HashSet::new();
+        // let mut cache_accounts = HashSet::new();
+        // let mut cache_instructions = HashSet::new();
 
-        {
-            let mut first_visitor = ComposeVisitor::new()
-                .add(CacheByImplsVisitor::new(&["Owner"], &mut cache_accounts))
-                .add(CacheInstructionIdents::new(&mut cache_instructions));
-            visitable.accept(&mut first_visitor)?;
-        }
+        // {
+        //     let mut first_visitor = ComposeVisitor::new()
+        //         .add(CacheByImplsVisitor::new(&["Owner"], &mut cache_accounts))
+        //         .add(CacheInstructionIdents::new(&mut cache_instructions));
+        //     visitable.accept(&mut first_visitor)?;
+        // }
 
-        let cache_accounts: Vec<String> = cache_accounts.into_iter().collect();
-        let cache_accounts_ref = &cache_accounts;
-        let _cache_instructions: Vec<String> = cache_instructions.into_iter().collect();
+        // let cache_accounts: Vec<String> = cache_accounts.into_iter().collect();
+        // let cache_accounts_ref = &cache_accounts;
+        // let _cache_instructions: Vec<String> = cache_instructions.into_iter().collect();
 
-        println!("{:?}", _cache_instructions);
+        // println!("{:?}", _cache_instructions);
 
-        let mut default_visitor = ComposeVisitor::new()
-            .add(FilterItemsVisitor::new(
-                move |item| {
-                    item.has_name_in_cache(cache_accounts_ref)
-                        || item.has_attribute("account")
-                        || item.has_attribute("context")
-                },
-                ComposeVisitor::new()
-                    .add(SetBorshTypesVisitor::new())
-                    .add(SetLinkTypesVisitor::new())
-                    .add(CombineTypesVisitor::new()),
-            ))
-            .add(FilterItemsVisitor::new(
-                move |item| {
-                    item.has_name_in_cache(cache_accounts_ref) || item.has_attribute("account")
-                },
-                UniformVisitor::new(|mut k, visitor| {
-                    visitor.visit_children(&mut k)?;
-                    apply_account(k);
-                    Ok(())
-                }),
-            ))
-            .add(SetProgramIdVisitor::new())
-            .add(SetProgramMetadataVisitor::new())
-            .add(CombineModulesVisitor::new());
+        // let mut default_visitor = ComposeVisitor::new()
+        //     .add(FilterItemsVisitor::new(
+        //         move |item| {
+        //             item.has_name_in_cache(cache_accounts_ref)
+        //                 || item.has_attribute("account")
+        //                 || item.has_attribute("context")
+        //         },
+        //         ComposeVisitor::new()
+        //             .add(SetBorshTypesVisitor::new())
+        //             .add(SetLinkTypesVisitor::new())
+        //             .add(CombineTypesVisitor::new()),
+        //     ))
+        //     .add(FilterItemsVisitor::new(
+        //         move |item| {
+        //             item.has_name_in_cache(cache_accounts_ref) || item.has_attribute("account")
+        //         },
+        //         UniformVisitor::new(|mut k, visitor| {
+        //             visitor.visit_children(&mut k)?;
+        //             apply_account(k);
+        //             Ok(())
+        //         }),
+        //     ))
+        //     .add(SetProgramIdVisitor::new())
+        //     .add(SetProgramMetadataVisitor::new())
+        //     .add(CombineModulesVisitor::new());
 
-        visitable.accept(&mut default_visitor)?;
+        // visitable.accept(&mut default_visitor)?;
 
         Ok(())
     }
 }
 
-fn apply_account(mut korok: KorokMut) {
-    let Some(Node::DefinedType(ref def_ty)) = korok.node() else {
-        return;
-    };
+// fn apply_account(mut korok: KorokMut) {
+//     let Some(Node::DefinedType(ref def_ty)) = korok.node() else {
+//         return;
+//     };
 
-    let Ok(data) = NestedTypeNode::<StructTypeNode>::try_from(def_ty.r#type.clone()) else {
-        return;
-    };
+//     let Ok(data) = NestedTypeNode::<StructTypeNode>::try_from(def_ty.r#type.clone()) else {
+//         return;
+//     };
 
-    let account = AccountNode {
-        name: def_ty.name.clone(),
-        docs: def_ty.docs.clone(),
-        size: None,
-        pda: None,
-        discriminators: Vec::new(),
-        data,
-    };
+//     let account = AccountNode {
+//         name: def_ty.name.clone(),
+//         docs: def_ty.docs.clone(),
+//         size: None,
+//         pda: None,
+//         discriminators: Vec::new(),
+//         data,
+//     };
 
-    korok.set_node(Some(Node::Account(account)));
-}
+//     korok.set_node(Some(Node::Account(account)));
+// }
