e7f14ef526d27ce024d945a4ff6169a807b1227e
Jean Marchand <jeanno11@orange.fr>
2025-03-26 11:49:50 +0700

    Refactor set program id

--- crates/metadata-extractor/src/visitors/set_program_id_visitor.rs
+++ crates/metadata-extractor/src/visitors/set_program_id_visitor.rs
@@ -10,7 +10,23 @@ pub struct SetProgramIdVisitor {
 
 impl SetProgramIdVisitor {
     pub fn new() -> Self {
-        SetProgramIdVisitor::default()
+        Self::default()
+    }
+
+    fn update_program_node(&mut self, program: &mut ProgramNode) {
+        if program.public_key.is_empty() {
+            if let Some(public_key) = &self.identified_public_key {
+                program.public_key = public_key.into();
+            }
+        }
+    }
+
+    fn handle_macro(&mut self, mac: &syn::Macro) {
+        if let ("" | "typhoon_program_id_macro", "program_id") =
+            (mac.path.prefix().as_str(), mac.path.last_str().as_str())
+        {
+            self.identified_public_key = Some(mac.tokens.to_string().replace("\"", ""));
+        }
     }
 }
 
@@ -18,45 +34,27 @@ impl KorokVisitor for SetProgramIdVisitor {
     fn visit_crate(&mut self, korok: &mut CrateKorok) -> CodamaResult<()> {
         self.visit_children(korok)?;
 
-        // Get a mutable reference to the program to update its metadata.
         let program = match &mut korok.node {
-            // Use the primary program of the root node if set.
             Some(Node::Root(root)) => &mut root.program,
-            // Use the existing program node if set.
             Some(Node::Program(program)) => program,
-            // If no node is set, create a new default program node.
             None => {
                 korok.node = Some(ProgramNode::default().into());
-                if let Some(Node::Program(program)) = &mut korok.node {
-                    program
-                } else {
-                    unreachable!()
+                match &mut korok.node {
+                    Some(Node::Program(program)) => program,
+                    _ => unreachable!(),
                 }
             }
-            // Don't update the node if it is set to anything else.
             _ => return Ok(()),
         };
 
-        if program.public_key.is_empty() {
-            if let Some(public_key) = &self.identified_public_key {
-                program.public_key = public_key.into()
-            }
-        }
-
+        self.update_program_node(program);
         Ok(())
     }
 
     fn visit_unsupported_item(&mut self, korok: &mut UnsupportedItemKorok) -> CodamaResult<()> {
-        let syn::Item::Macro(syn::ItemMacro { mac, .. }) = korok.ast else {
-            return Ok(());
-        };
-
-        if let ("" | "typhoon_program_id_macro", "program_id") =
-            (mac.path.prefix().as_str(), mac.path.last_str().as_str())
-        {
-            self.identified_public_key = Some(mac.tokens.to_string().replace("\"", ""));
-        };
-
+        if let syn::Item::Macro(syn::ItemMacro { mac, .. }) = korok.ast {
+            self.handle_macro(mac);
+        }
         Ok(())
     }
 }
