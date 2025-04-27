07ff80d418e5ee589e44d6774bbc709793a35ddf
Jean Marchand <jeanno11@orange.fr>
2025-04-27 18:22:18 +0700

    Rework error crate

---
+++ crates/errors/src/default_custom.rs
@@ -0,0 +1,31 @@
+use std::fmt::Display;
+
+use num_traits::{FromPrimitive, ToPrimitive};
+
+pub struct CustomError;
+
+impl Display for CustomError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "")
+    }
+}
+
+impl FromPrimitive for CustomError {
+    fn from_i64(_n: i64) -> Option<Self> {
+        None
+    }
+
+    fn from_u64(_n: u64) -> Option<Self> {
+        None
+    }
+}
+
+impl ToPrimitive for CustomError {
+    fn to_i64(&self) -> Option<i64> {
+        None
+    }
+
+    fn to_u64(&self) -> Option<u64> {
+        None
+    }
+}
