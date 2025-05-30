1199d051f1df953df788a0317883b6616531ef8a
Jean Marchand <jeanno11@orange.fr>
2025-05-30 19:06:30 +0200

    Add one exemple of the idl generation

--- crates/idl-generator/src/main.rs
+++ crates/idl-generator/src/main.rs
@@ -1,11 +1,6 @@
-use {codama::Codama, std::path::Path, typhoon_idl_generator::plugin::TyphoonPlugin};
+use typhoon_idl_generator::generate;
 
 pub fn main() {
-    // let manifest_dir = env!("CARGO_MANIFEST_DIR");
-    let p = Path::new("/home/aursen/crayfish/examples/instruction-data");
-    let codama = Codama::load(p)
-        .unwrap()
-        .without_default_plugin()
-        .add_plugin(TyphoonPlugin);
-    println!("{}", codama.get_json_idl().unwrap());
+    let manifest_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+    println!("{}", generate(manifest_dir).unwrap());
 }
