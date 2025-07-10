d2cf3c7b962b1ec8dd4eea666fb84ea989b3aa6f
Kernlog <0xkernlog@gmail.com>
2025-07-10 11:36:32 -0400

    chore: inline handle function - drops ping cu back to 11

--- crates/context/src/lib.rs
+++ crates/context/src/lib.rs
@@ -85,6 +85,7 @@ impl_handler!(T1, T2, T3, T4, T5);
 impl_handler!(T1, T2, T3, T4, T5, T6);
 impl_handler!(T1, T2, T3, T4, T5, T6, T7);
 
+#[inline(always)]
 pub fn handle<'a, 'b, 'c, T, H>(
     program_id: &'a Pubkey,
     mut accounts: &'b [AccountInfo],
@@ -99,7 +100,6 @@ where
             if core::mem::size_of::<H::Output>() > 0 {
                 set_return_data(bytemuck::bytes_of(&res));
             }
-
             Ok(())
         }
         Err(err) => Err(err),
