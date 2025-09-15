90be942da3682a585c596a9696c3b5269806a245
Aursen <aursen@users.noreply.github.com>
2025-09-15 20:25:00 +0700

    Fix token

--- crates/context-macro/src/generators/account.rs
+++ crates/context-macro/src/generators/account.rs
@@ -114,7 +114,7 @@ impl AccountGenerator<'_> {
                     let inner_ty = format_ident!("{}", self.account.inner_ty);
                     quote!(#inner_ty::derive(#keys))
                 } else {
-                    quote!([keys])
+                    quote!([#keys])
                 };
                 Ok(quote! {
                     let (_, #pda_bump) = find_program_address(&#seeds_token, &#program_id);
