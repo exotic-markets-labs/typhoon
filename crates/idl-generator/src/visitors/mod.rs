96916564edeae4f3187e7ad2335f134eeea7ec0e
Aursen <aursen@users.noreply.github.com>
2026-02-17 11:25:54 +0700

    fix: idl only include needed type

--- crates/idl-generator/src/visitors/mod.rs
+++ crates/idl-generator/src/visitors/mod.rs
@@ -1,12 +1,12 @@
 mod context_visitor;
+mod define_type_resolver;
 mod instruction_resolver;
 mod router_visitor;
 mod set_account_visitor;
-mod set_defined_types;
 mod set_errors_visitor;
 mod set_program_id_visitor;
 
 pub use {
-    context_visitor::*, instruction_resolver::*, router_visitor::*, set_account_visitor::*,
-    set_defined_types::*, set_errors_visitor::*, set_program_id_visitor::*,
+    context_visitor::*, define_type_resolver::*, instruction_resolver::*, router_visitor::*,
+    set_account_visitor::*, set_errors_visitor::*, set_program_id_visitor::*,
 };
