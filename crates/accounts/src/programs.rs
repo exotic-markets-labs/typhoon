98a5f8d446b8b4fdf19505ceae7ee29c19d382eb
Aursen <aursen@users.noreply.github.com>
2026-03-03 20:44:56 +0700

    fix: improve address comparison logic for system program_id

--- crates/accounts/src/programs.rs
+++ crates/accounts/src/programs.rs
@@ -1,13 +1,18 @@
-use {
-    solana_address::{address_eq, Address},
-    typhoon_traits::CheckProgramId,
-};
+use {solana_address::Address, typhoon_traits::CheckProgramId};
 
 pub struct System;
 
 impl CheckProgramId for System {
     #[inline(always)]
     fn address_eq(program_id: &Address) -> bool {
-        address_eq(program_id, &Address::new_from_array([0; 32]))
+        let p = program_id.as_array().as_ptr().cast::<u64>();
+
+        unsafe {
+            core::ptr::read_unaligned(p)
+                | core::ptr::read_unaligned(p.add(1))
+                | core::ptr::read_unaligned(p.add(2))
+                | core::ptr::read_unaligned(p.add(3))
+                == 0
+        }
     }
 }
